7744aa5ce85da4b5de2446e703a6f019785cecce
darshanpatel5758 <darshanpatel5758@gmail.com>
2020-09-27 00:41:26 -0400

    frist commit for deliverable one file name program.cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+            string s = "!";
+            int count = 0;
+            int count1 = 0;
+
+
+            for (int i = 0; i < password.Length; i++)
+            {
+            
+                if (char.IsUpper(password[i])) count++;
+                if (char.IsLower(password[i])) count1++;
+                
+            }
+
+            if (password.Contains(s))
+            {
+                if (password.Length < 7 || password.Length > 12 || count < 1 || count1 < 1)
+                {
+                    Console.WriteLine("Error");
+                }
+                else
+                {
+                    Console.WriteLine("Password valid and accepted");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+
+        }
+    }
+}
